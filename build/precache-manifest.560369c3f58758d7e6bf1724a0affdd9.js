2a64b0025be3fe9d972a3983ef075fe4def0e1d7
RobertLBebber <robert.l.bebber@gmail.com>
2020-03-29 23:29:20 -0400

    More Tweaks

--- build/precache-manifest.560369c3f58758d7e6bf1724a0affdd9.js
+++ build/precache-manifest.560369c3f58758d7e6bf1724a0affdd9.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     revision: "d077ab72df9c8fef7b42",
-    url: "./static/css/main.9f42bfe2.chunk.css"
+    url: "./build/static/css/main.9f42bfe2.chunk.css"
   },
   {
     revision: "d077ab72df9c8fef7b42",
-    url: "./static/js/main.d077ab72.chunk.js"
+    url: "./build/static/js/main.d077ab72.chunk.js"
   },
   {
     revision: "fdfcfda2d9b1bf31db52",
-    url: "./static/js/runtime~main.fdfcfda2.js"
+    url: "./build/static/js/runtime~main.fdfcfda2.js"
   },
   {
     revision: "dd9936e4aff9b4452a4f",
-    url: "./static/js/2.dd9936e4.chunk.js"
+    url: "./build/static/js/2.dd9936e4.chunk.js"
   },
   {
     revision: "bb107ab71625ebd2582b408259139182",
-    url: "./index.html"
+    url: "./build/index.html"
   }
 ];
